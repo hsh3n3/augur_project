087c03f9f70bd4e80b1427938cad223e91917a03
Micah Tamerius <motc34@umsystem.edu>
2021-11-20 14:21:40 -0600

    take 4

--- augur/metrics/deps1.py
+++ augur/metrics/deps1.py
@@ -2,7 +2,7 @@
 """
 Metrics that provide data about with insight detection and reporting
 """
-
+import datetime
 import sqlalchemy as s
 import pandas as pd
 from augur.util import register_metric
@@ -22,26 +22,9 @@ def deps1(self):
     :param repo_group_id: The repository's group id
     :return: DataFrame of persons/period
     """
-	# if repo_id:
-	# 	depsSQL = s.sql.text("""
-	# 		SELECT repo_dependencies.repo_id, data_source, dep_name, dep_language, max(data_collection_date)
-	# 		FROM repo_dependencies, (select repo_id, max(data_collection_date) as thedate from repo_dependencies group by repo_id) a 
-	# 		where a.repo_id = repo_dependencies.repo_id and a.thedate=repo_dependencies.data_collection_date
-	# 		group by repo_dependencies.repo_id, data_source, dep_name, dep_language
-	# 		ORDER BY repo_id, dep_language, dep_name;
-	# 	""")
-	# 	results = pd.read_sql(depsSQL, self.database, params={'repo_id'})
+	depsSQL = s.sql.text("""
+			SELECT dep_name FROM repo_dependencies
+		""")
+	results = pd.read_sql(depsSQL, self.database, params={'repo_id'})
 
-	# else:
-	# 	depsSQL = s.sql.text("""
-	# 		SELECT repo_dependencies.repo_id, data_source, dep_name, dep_language, max(data_collection_date)
-	# 		FROM repo_dependencies, (select repo_id, max(data_collection_date) as thedate from repo_dependencies group by repo_id) a 
-	# 		where a.repo_id = repo_dependencies.repo_id and a.thedate=repo_dependencies.data_collection_date
-	# 		group by repo_dependencies.repo_id, data_source, dep_name, dep_language
-	# 		ORDER BY repo_id, dep_language, dep_name;
-	# 	""")
-	# 	results = pd.read_sql(depsSQL, self.database)
-
-	# return results
-
-	return "Hello World"
+	return results
