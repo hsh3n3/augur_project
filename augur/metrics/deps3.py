d5257965c5e56b8a825d15b2668efcf595939989
Micah Tamerius <motc34@umsystem.edu>
2021-12-02 15:42:49 -0600

    more deps tests

--- augur/metrics/deps3.py
+++ augur/metrics/deps3.py
@@ -9,12 +9,12 @@ from augur.util import register_metric
 from flask import request, Response, Flask
 
 @register_metric()
-def deps3(self):
+def deps3(self, repo_group_id):
   
 
     contributorsSQL = s.sql.text("""
         SELECT dep_name FROM repo_dependencies
     """)
 
-    results = pd.read_sql(contributorsSQL, self.database)
+    results = pd.read_sql(contributorsSQL, self.database, params={'repo_group_id': repo_group_id})
     return results
--- augur/metrics/deps4.py
+++ augur/metrics/deps4.py
@@ -9,10 +9,11 @@ from augur.util import register_metric
 from flask import request, Response, Flask
 
 @register_metric()
-def deps2(self, repo_group_id):
-    
+def deps3(self, repo_group_id):
+  
+
     contributorsSQL = s.sql.text("""
-        SELECT dep_name FROM repo_dependencies
+        SELECT dep_name, dep_count FROM repo_dependencies ORDER BY dep_count DESC;
     """)
 
     results = pd.read_sql(contributorsSQL, self.database, params={'repo_group_id': repo_group_id})
