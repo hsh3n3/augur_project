fccdce0a3dcd7d193574db5c9a5d29cf9d185784
Micah Tamerius <motc34@umsystem.edu>
2021-12-02 15:08:47 -0600

    added deps3

---
+++ augur/metrics/deps3.py
@@ -0,0 +1,19 @@
+#SPDX-License-Identifier: MIT
+"""
+Metrics that provide data about with insight detection and reporting
+"""
+import datetime
+import sqlalchemy as s
+import pandas as pd
+from augur.util import register_metric
+from flask import request, Response, Flask
+
+@register_metric()
+def deps3(self):
+  
+    contributorsSQL = s.sql.text("""
+        SELECT dep_name FROM repo_dependencies
+    """)
+
+    results = pd.read_sql(contributorsSQL, self.database)
+    return results
