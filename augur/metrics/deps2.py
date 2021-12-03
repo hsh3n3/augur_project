70cc36629595b92e7c1296d640296382734633a8
Micah Tamerius <motc34@umsystem.edu>
2021-12-03 15:20:32 -0600

    last test i promise

--- augur/metrics/deps2.py
+++ augur/metrics/deps2.py
@@ -10,10 +10,15 @@ from flask import request, Response, Flask
 
 @register_metric()
 def deps2(self, repo_id):
+    """
+    Returns the name of every dependency of all loaded repos
+    DataFrame has these columns:
+	dep_name
+    """
 
     contributorsSQL = s.sql.text("""
-        SELECT dep_name FROM repo_dependencies
+        SELECT dep_name FROM repo_dependencies WHERE repo_id = :repo_id
     """)
 
-    results = pd.read_sql(contributorsSQL, self.database)
+    results = pd.read_sql(contributorsSQL, self.database, params={'repo_id': repo_id})
     return results
--- augur/metrics/deps3.py
+++ augur/metrics/deps3.py
@@ -9,16 +9,22 @@ from augur.util import register_metric
 from flask import request, Response, Flask
 
 @register_metric()
-def deps3(self, repo_group_id):
+def deps3(self, repo_group_id, repo_id = None):
     """
     Returns the name of every dependency of all loaded repos
     DataFrame has these columns:
 	dep_name
     """
+    if repo_id:
+        contributorsSQL = s.sql.text("""
+            SELECT dep_name FROM repo_dependencies WHERE repo_id = :repo_id
+        """)
+        results = pd.read_sql(contributorsSQL, self.database, params={'repo_id': repo_id})
+    else:
+        contributorsSQL = s.sql.text("""
+            SELECT dep_name FROM repo_dependencies WHERE repo_id = :repo_group_id
+        """)
+        results = pd.read_sql(contributorsSQL, self.database, params={'repo_group_id': repo_group_id})
 
-    contributorsSQL = s.sql.text("""
-        SELECT dep_name FROM repo_dependencies WHERE repo_id = :repo_group_id
-    """)
-
-    results = pd.read_sql(contributorsSQL, self.database, params={'repo_group_id': repo_group_id})
+    
     return results
